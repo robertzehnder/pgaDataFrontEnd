3ac5e25e94ddce14b27a1c0f513ba6094e0e10d9
Robert Zehnder <robzcodes@gmail.com>
2017-01-04 21:43:15 -0500

    firstFrontEnd commit

---
+++ index.js
@@ -0,0 +1,46 @@
+var express = require("express");
+var elasticsearch = require('elasticsearch')
+
+var app = express();
+
+var client = elasticsearch.Client({
+  host: 'https://elastic:sK6vNw9d5JrvzsVb4zv1v07K@52e10366c0a54c2ae88a2ece0b28cc1b.us-east-1.aws.found.io:9243' 
+})
+
+app.listen(4000, () => {
+  console.log("app listening on port 4000");
+});
+
+app.set("view engine", "hbs");
+
+app.get("/", (req, res) => {
+  res.render('index')
+})
+
+app.get("/analytics", (req, res) => {
+  res.render('dashboard')
+})
+
+app.get("/about", (req, res) => {
+  res.render('about')
+})
+
+app.get("/:name", (req, res) => {
+  client.search({
+    index: req.params.name,
+    type: 'stats',
+    body: {
+      size: 1000,
+      query: {
+        match_all: {}
+        }
+      }
+  }).then(function (response) {
+    res.json(response);
+    // var hits = response.hits.hits
+  }, function (error) {
+    console.trace(error.message)
+  })
+});
+
+app.get("/_cat/indices?v")
